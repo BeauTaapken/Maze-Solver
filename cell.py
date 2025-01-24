c17acf5de2d65741d99a012662d88057b02455e3
Beau Taapken <beau@lioncode.nl>
2025-01-24 08:55:13 +0100

    generating sttuff

---
+++ cell.py
@@ -0,0 +1,64 @@
+from line import Line
+from point import Point
+
+
+class Cell():
+    def __init__(self, win=None) -> None:
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self.win = win
+
+    def draw(self, x1, y1, x2, y2):
+        if not self.win:
+            return
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+
+        self._draw_wall(Point(self._x1, self._y1), Point(self._x1, self._y2), self.has_left_wall)
+
+        self._draw_wall(Point(self._x2, self._y1), Point(self._x2, self._y2), self.has_right_wall)
+
+        self._draw_wall(Point(self._x1, self._y1), Point(self._x2, self._y1), self.has_top_wall)
+
+        self._draw_wall(Point(self._x1, self._y2), Point(self._x2, self._y2), self.has_bottom_wall)
+        #if self.has_left_wall:
+        #    line = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+        #    self.win.draw_line(line, "black")
+        #if self.has_right_wall:
+        #    line = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+        #    self.win.draw_line(line, "black")
+        #if self.has_top_wall:
+        #    line = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
+        #    self.win.draw_line(line, "black")
+        #if self.has_bottom_wall:
+        #    line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
+        #    self.win.draw_line(line, "black")
+
+    def _draw_wall(self, start, end, draw_visible_wall):
+        color = "black" if draw_visible_wall else "white"
+        line = Line(start, end)
+        self.win.draw_line(line, color)
+
+    def draw_move(self, to_cell, undo=False):
+        if not self.win:
+            return
+        color = "gray" if undo else "red"
+
+        half_length = abs(self._x2 - self._x1) // 2
+
+        half_length2 = abs(to_cell._x2 - to_cell._x1) // 2
+
+        center_cur_cell = (half_length + self._x1, half_length + self._y1)
+        center_to_cell = (half_length2 + to_cell._x1, half_length2 + to_cell._y1)
+
+        line = Line(Point(center_cur_cell[0], center_cur_cell[1]), Point(center_to_cell[0], center_to_cell[1]))
+        self.win.draw_line(line, color)
+
---
+++ window.py
@@ -0,0 +1,28 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window():
+    def __init__(self, width, height) -> None:
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+        self.canvas = Canvas(self.__root, bg="white", width=width, height=height)
+        self.canvas.pack(fill=BOTH, expand=1)
+
+        self.running = False
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+
+    def close(self):
+        self.running = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.canvas, fill_color)
+
